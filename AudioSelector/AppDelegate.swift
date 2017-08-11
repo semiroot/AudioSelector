04d3d21e852a819e1d5c98a55f84f4382c29de99
Hansmartin Geiser <hgeiser@little-bit.net>
2017-08-11 20:11:16 +0200

    Initial Commit

---
+++ AudioSelector/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AudioSelector
+//
+//  Created by Semiotikus on 11.08.17.
+//  Copyright © 2017 Hansmartin Geiser. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AudioSelector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AudioSelector
+//
+//  Created by Semiotikus on 11.08.17.
+//  Copyright © 2017 Hansmartin Geiser. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
